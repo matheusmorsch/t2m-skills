22d702150025d8d4e6a68757c52622c93a2dfd0f
Bruna do Valle Sóre <brunadovallesore@gmail.com>
2021-07-20 09:06:25 -0300

    Criação de classes e interface de Categoria.

---
+++ src/main/java/com/t2m/skills/vo/CategoriaInserirVO.java
@@ -0,0 +1,18 @@
+package com.t2m.skills.vo;
+
+public class CategoriaInserirVO {
+		private String nome;		
+		
+		public CategoriaInserirVO(String nome) {
+			super();
+			this.nome = nome;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+	}
+
--- src/main/java/com/t2m/skills/vo/CategoriasVO.java
+++ src/main/java/com/t2m/skills/vo/CategoriasVO.java
@@ -1,5 +1,11 @@
 package com.t2m.skills.vo;
 
+import com.t2m.skills.entities.Categorias;
+
 public class CategoriasVO {
+
+	public CategoriasVO(Categorias save) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
